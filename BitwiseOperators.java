479bb65fbd98ab83c1229fd1804cface90fe99a8
Ratnesh Kumar <ratneshkumar8699@gmail.com>
2025-06-21 00:21:54 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,17 @@
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        int p = 9, q = 10;
+
+        System.out.println(p|q);
+        System.out.println(p&q);
+        System.out.println(p^q);
+        System.out.println(p<<1);
+        System.out.println(p<<2);
+        System.out.println(q<<1);
+        System.out.println(q<<2);
+        System.out.println(p>>1);
+        System.out.println(p>>2);
+        System.out.println(q>>1);
+        System.out.println(q>>2);
+    }
+}
